6c955892dd06b4fcb55f26c7351ebcf486656b4d
zaboevai <zaboevai@yandex.ru>
2019-08-15 14:10:46 +0300

    remove console_parser and output

--- hw_01/analyze_projects.py
+++ hw_01/analyze_projects.py
@@ -1,46 +1,31 @@
 import os
 import collections
-import argparse
 
+from utils.output import output_to_console, save_to_json_file
 from utils.word_analyser import get_top_words
 from utils.github import clone_github_repo
-from utils.console_user_requests import clone_github_request
+from utils.console import clone_github_request, console_parser
 
 
-def print_calc_result(_project, verb_size, words):
-    print('%s: total %s words, %s unique' % (_project, len(words), len(set(words))))
-    for word, occurence in collections.Counter(words).most_common(verb_size):
-        print(word, occurence)
-
-
-def calc_verbs(_project: str, word_size: int, only_func_names=False, only_verbs=False):
+def calc_words_in_project(_project: str, word_size: int, only_func_names=False, only_verbs=False, to_json=False):
     project_path = os.path.join('.', _project)
     if not os.path.exists(project_path):
         return
 
-    words = get_top_words(project_path, only_func_names=only_func_names, only_verbs=only_verbs)
-    if words:
-        print_calc_result(project_path, word_size, words)
+    top_words = get_top_words(project_path, only_func_names=only_func_names, only_verbs=only_verbs)
+    if top_words:
+        words = {}
+        for word, occurence in collections.Counter(top_words).most_common(word_size):
+            words[word] = occurence
 
-
-def console_parser():
-    parser = argparse.ArgumentParser(description='Analyze project code by words and verbs.', add_help=True)
-    parser.add_argument('path', type=str, default='', help='Path to project')
-    parser.add_argument('-fn', action='store_true', help='Calculate words in function names')
-    parser.add_argument('-v', action='store_true', help='Calculate verbs')
-    args = parser.parse_args()
-    return args
+        if to_json:
+            save_to_json_file(_project, words)
+        else:
+            output_to_console(_project, words)
 
 
 if __name__ == '__main__':
     # TODO Need add some logging
-
-    clone_github_answer = clone_github_request()
-
-    if clone_github_answer:
-        print(f'Репозиторий клонируется...')
-        clone_github_repo(clone_github_answer['rep_link'], clone_github_answer['dir'])
-        print(f'Клонирование успешно завершено!')
 
     TOP_SIZE = 200
 
@@ -52,20 +37,28 @@ if __name__ == '__main__':
         'requests',
         'sqlalchemy',
     ]
-    user_answer = input('Провести анализ кода проекта ? (y/n)')
-    if user_answer.lower() not in {'y','да'}:
-        exit()
 
     console_args = console_parser()
-
-    print(f'Analyze path={console_args.path}, -fn={console_args.fn}, -v={console_args.v}.')
 
     if console_args.path:
         projects.clear()
         projects.append(os.path.basename(console_args.path))
 
+    user_answer = input('Провести анализ кода проекта ? (y/n)')
+
+    if user_answer.lower() not in {'y', 'да'}:
+        exit()
+
+    clone_github_answer = clone_github_request()
+
+    if clone_github_answer:
+        print(f'Репозиторий клонируется...')
+        clone_github_repo(clone_github_answer['rep_link'], clone_github_answer['dir'])
+        print(f'Клонирование успешно завершено!')
+
     for project in projects:
-        calc_verbs(_project=project,
-                   word_size=TOP_SIZE,
-                   only_func_names=console_args.fn,
-                   only_verbs=console_args.v)
+        calc_words_in_project(_project=project,
+                              word_size=TOP_SIZE,
+                              only_func_names=console_args.fn,
+                              only_verbs=console_args.v,
+                              to_json=True)
